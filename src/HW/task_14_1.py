171b6c66f0a765dbe0ee4b8b7e5a2904e3681b65
Andrei <Andrei>
2020-09-07 21:02:25 +0300

    sum stuf

--- src/HW/task_14_1.py
+++ src/HW/task_14_1.py
@@ -1,14 +1,14 @@
 import argparse
-from src.HW12.task_12_1 import MyTime
+from ..HW12.task_12_1 import MyTime
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-fn', '--first-name', required=True)
-parser.add_argument('-ln', '--last-name', required=True)
-parser.add_argument('-t', '--time', required=True)
+# parser.add_argument('-fn', '--first-name', required=True)
+# parser.add_argument('-ln', '--last-name', required=True)
+# parser.add_argument('-t', '--time', required=True)
+#
+# args = parser.parse_args()
+# print(args)
 
-args = parser.parse_args()
-print(args)
-
-second =  MyTime('00:00:01')
+second = MyTime('00:00:01')
 time = MyTime()
