1563f367e905edbca34a740b7e2008b2832f19f6
Andrei <Andrei>
2020-09-07 18:18:48 +0300

    trying import from HW12

--- src/HW/task_14_1.py
+++ src/HW/task_14_1.py
@@ -1,3 +1,14 @@
 import argparse
+from src.HW12.task_12_1 import MyTime
+
 parser = argparse.ArgumentParser()
-parser.parse_args()
+
+parser.add_argument('-fn', '--first-name', required=True)
+parser.add_argument('-ln', '--last-name', required=True)
+parser.add_argument('-t', '--time', required=True)
+
+args = parser.parse_args()
+print(args)
+
+second =  MyTime('00:00:01')
+time = MyTime()
