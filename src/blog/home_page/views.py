b453aa33b5d035a64b4a4d743a53a380477dc843
AndreiKotM <andrew.m.kot@gmail.com>
2020-09-23 17:19:26 +0300

    new changes

--- src/blog/home_page/views.py
+++ src/blog/home_page/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import *
 
 def index(requests):
-    return HttpResponse(f'This is a home page! {say_hello("User")}')
+    # return HttpResponse(f'This is a home page! {say_hello("User")}')
+    name = "USER"
+    return render(requests, 'home_page/index.html', locals())
